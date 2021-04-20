c4069e0f944bf6c35c1efda00b65da3139d7f8ec
matsjs <matsjorgen@hotmail.com>
2021-04-20 14:39:39 +0200

    fix: adapt home link to env

--- nodebb-theme-fdk/lib/client.js
+++ nodebb-theme-fdk/lib/client.js
@@ -1,9 +1,9 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-	$('main').on( "DOMNodeInserted", function( e ) {
-		if(window.location.pathname !== '/') {
+	$('main').on("DOMNodeInserted", function (e) {
+		if (window.location.pathname !== '/') {
 			let bc = $('main > div > ol.breadcrumb')[0];
-			if(bc) {
+			if (bc) {
 				$('div.fdk-breadcrumbs-container').empty();
 				$('div.fdk-breadcrumbs-container')[0].appendChild(bc);
 				$('div.fdk-breadcrumbs').show();
@@ -13,16 +13,56 @@ $(document).ready(function() {
 		}
 	});
 
-	(function(history){
-    var pushState = history.pushState;
-    history.pushState = function(state) {
-			if (arguments[0].url !== undefined && arguments[0].url === "") {
-				document.getElementById("fdk-splash").style.display = "inherit";
-			} else {
-				document.getElementById("fdk-splash").style.display = "none";
-			}
-      return pushState.apply(history, arguments);
-    };
-})(window.history);
+	function toggleSplash(path) {
+		if (path === "/") {
+			document.getElementById("fdk-splash").style.display = "inherit";
+		} else {
+			document.getElementById("fdk-splash").style.display = "none";
+		}
+	}
+
+	window.addEventListener('popstate', function () {toggleSplash(location.pathname)});
+
+	(function (history) {
+		var pushState = history.pushState;
+		history.pushState = function (data, title, url) {
+			toggleSplash(url)
+			return pushState.apply(history, [data, title, url]);
+		};
+
+		toggleSplash(location.pathname)
+	})(window.history);
+
+	function getEnvironment() {
+		const host = window.location.hostname.split(".")
+
+		if (host.includes("staging") || host.includes("localhost")) {
+			return "staging";
+		} else if (host.includes("demo")) {
+			return "demo";
+		}
+		return "prod";
+	}
+
+	function setHomeLink() {
+		const environment = getEnvironment();
+		var href = "";
+		switch (environment) {
+			case "demo":
+				href = "https://demo.fellesdatakatalog.digdir.no/";
+				break;
+			case "prod":
+				href = "https://data.norge.no/";
+				break;
+			case "staging":
+			default:
+				href = "https://www.staging.fellesdatakatalog.digdir.no/";
+				break;
+		}
+		document.getElementById("fdk-logo-link").href = href;
+	}
+
+
+	setHomeLink();
 });
 
