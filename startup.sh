da2b91bc2f41aea5678e5d65edcde8f15952b365
Jeff Reiffers <jeffreiffers@gmail.com>
2021-06-14 09:48:13 +0200

    chore: run ssmtp setup first

--- startup.sh
+++ startup.sh
@@ -2,14 +2,14 @@
 
 echo "Community Docker container has been started"
 
+/setup-ssmtp.sh
+
 # Setup a cron schedule (every hour)
 echo "0 * * * * /run.sh >> /var/log/cron.log 2>&1
 # This extra line makes it a valid cron" > scheduler.txt
 
 crontab scheduler.txt
 cron -f &
-
-/setup-ssmtp.sh
 
 node ./nodebb activate nodebb-plugin-fdk-sso
 node ./nodebb activate nodebb-plugin-calendar
