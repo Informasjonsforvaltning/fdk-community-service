dc9f2118ee4839223bdc8b82cd554e4a6fc54af9
Jeff Reiffers <jeffreiffers@gmail.com>
2021-06-17 08:49:42 +0200

    feat: send email when user is about to be deleted or is deleted

--- run.sh
+++ run.sh
@@ -1,5 +1,55 @@
 #!/bin/bash
 
+FILES_DIR="/usr/src/app/files"
+
+sendUserDeletedEmail() {
+  uid=$1
+  flagFile="$FILES_DIR/mail/deleted-$uid"
+
+  mkdir -p "$FILES_DIR/mail"
+
+  if [ ! -f "$flagFile" ]; then
+    echo "$ts - Sending email to user with uid $uid"
+
+    userslug=$2
+    name=$3
+    email=$(curl -s -H "Authorization: Bearer $API_TOKEN" "http://localhost:4567/api/user/$userslug" | jq '.email')
+
+    mail=$(cat /mail-template-deleted.html)
+    mail="${mail/@@BASE_URL@@/$BASE_URL}"
+    mail="${mail/@@UID@@/$uid}"
+    mail="${mail/@@EMAIL@@/$email}"
+    mail="${mail/@@NAME@@/$name}"
+
+    echo $mail | sendmail $to
+    touch $flagFile
+  fi
+}
+
+sendDeleteUserInTenDaysEmail() {
+  uid=$1
+  flagFile="$FILES_DIR/mail/delete-10days-$uid"
+
+  mkdir -p "$FILES_DIR/mail"
+
+  if [ ! -f "$flagFile" ]; then
+    echo "$ts - Sending email to user with uid $uid"
+
+    userslug=$2
+    name=$3
+    email=$(curl -s -H "Authorization: Bearer $API_TOKEN" "http://localhost:4567/api/user/$userslug" | jq '.email')
+
+    mail=$(cat /mail-template-delete-7days.html)
+    mail="${mail/@@BASE_URL@@/$BASE_URL}"
+    mail="${mail/@@UID@@/$uid}"
+    mail="${mail/@@EMAIL@@/$email}"
+    mail="${mail/@@NAME@@/$name}"
+
+    echo $mail | sendmail $to
+    touch $flagFile
+  fi
+}
+
 ts=`date +%Y/%m/%d-%H:%M:%S`
 
 echo "$ts - Removing inactive users or users without consent"
@@ -12,6 +62,7 @@ for row in $(echo "${users}" | jq -r '.[] | @base64'); do
 
 	uid=$(_jq '.uid')
 	userslug=$(_jq '.userslug')
+	displayname=$(_jq '.displayname')
 	lastonline=$(_jq '.lastonline')
 	joindate=$(_jq '.joindate')
 
@@ -26,7 +77,15 @@ for row in $(echo "${users}" | jq -r '.[] | @base64'); do
 		echo "$ts - Removing inactive user with uid $uid (not online for $diff days)"
 		curl -s -H "Authorization: Bearer $API_TOKEN_WRITE" -X DELETE "http://localhost:4567/api/v2/users/$uid"
 		echo ""
+
+		sendUserDeletedEmail "$uid" "$userslug" "$displayname"
 	fi
+
+	# Send email if users are going to removed in exactly 10 days
+	if [ $diff_lastonline -ge 358 ]
+	then
+	  sendDeleteUserInTenDaysEmail "$uid" "$userslug" "$displayname"
+  fi
 
   echo "$ts - Verifying if user with uid $uid has approved gdpr consent..."
   echo "$ts - User with uid $uid joined: $diff_joindate hours ago"
