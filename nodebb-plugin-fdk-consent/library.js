9e622dda5c59614b0fd87782f8909707cc5dcdea
pooriamehregan <pooriamehregan@gmail.com>
2022-06-02 17:22:40 +0200

    fix: new clean-ups

--- nodebb-plugin-fdk-consent/library.js
+++ nodebb-plugin-fdk-consent/library.js
@@ -6,10 +6,10 @@ const winston = require.main.require('winston');
 const plugin = {};
 
 plugin.init = async (params) => {
-	const { router, middleware } = params;
+	const { router } = params;
 	const routeHelpers = require.main.require('./src/routes/helpers');
 
-	routeHelpers.setupPageRoute(router, '/consent', middleware, [(req, res, next) => {
+	routeHelpers.setupPageRoute(router, '/consent', [(req, res, next) => {
 		winston.info(`[plugins/consent] In middleware. This argument can be either a single middleware or an array of middlewares`);
 		setImmediate(next);
 	}], (req, res) => {
