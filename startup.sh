47290cc6d642086845e7b8c8c00ae8516c99fae7
Jeff Reiffers <jeffreiffers@gmail.com>
2022-11-14 10:11:13 +0100

    chore: add token uid

--- startup.sh
+++ startup.sh
@@ -5,7 +5,7 @@ echo "Community Docker container has been started"
 /setup-msmtp.sh
 
 # Setup a cron schedule (every hour)
-echo "*/5 * * * * TEST_MODE=$TEST_MODE TEST_EMAIL=$TEST_EMAIL API_TOKEN=$API_TOKEN API_TOKEN_WRITE=$API_TOKEN_WRITE BASE_URL='https://community.staging.fellesdatakatalog.digdir.no' /run.sh >> /usr/src/app/files/log/cron.log 2>&1
+echo "*/5 * * * * TEST_MODE=$TEST_MODE TEST_EMAIL=$TEST_EMAIL API_TOKEN=$API_TOKEN API_TOKEN_WRITE=$API_TOKEN_WRITE TOKEN_UID=$TOKEN_UID BASE_URL='https://community.staging.fellesdatakatalog.digdir.no' /run.sh >> /usr/src/app/files/log/cron.log 2>&1
 # This extra line makes it a valid cron" > scheduler.txt
 
 crontab scheduler.txt
