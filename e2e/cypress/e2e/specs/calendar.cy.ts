df051ff56134673fb4aa309379ac37a8455c7e2e
Hege Aalvik <hegeaal@gmail.com>
2023-01-31 10:16:09 +0100

    fix: make test run

--- e2e/cypress/e2e/specs/calendar.cy.ts
+++ e2e/cypress/e2e/specs/calendar.cy.ts
@@ -3,14 +3,14 @@ describe('Datalandsbyen', function() {
     it('test if all categories are listed', function() {
       cy.visit('http://localhost:4567/calendar');
       
-      cy.get('.fc-day-header.fc-widget-header').children().should('have.length', 7);
-      cy.get('.fc-day-header.fc-widget-header:first').should('to.contain', 'Mon');
-      cy.get('.fc-day-header.fc-widget-header:nth-child(2)').should('to.contain', 'Tue');
-      cy.get('.fc-day-header.fc-widget-header:nth-child(3)').should('to.contain', 'Wed');
-      cy.get('.fc-day-header.fc-widget-header:nth-child(4)').should('to.contain', 'Thu');
-      cy.get('.fc-day-header.fc-widget-header:nth-child(5)').should('to.contain', 'Fri');
-      cy.get('.fc-day-header.fc-widget-header:nth-child(6)').should('to.contain', 'Sat');
-      cy.get('.fc-day-header.fc-widget-header:nth-child(7)').should('to.contain', 'Sun');
+      cy.get('.fc-day-header').children().should('have.length', 7);
+      cy.get('.fc-day-header.fc-mon').should('to.contain', 'Mon');
+      cy.get('.fc-day-header.fc-tue:nth-child(2)').should('to.contain', 'Tue');
+      cy.get('.fc-day-header.fc-wed:nth-child(3)').should('to.contain', 'Wed');
+      cy.get('.fc-day-header.fc-thu:nth-child(4)').should('to.contain', 'Thu');
+      cy.get('.fc-day-header.fc-fri:nth-child(5)').should('to.contain', 'Fri');
+      cy.get('.fc-day-header.fc-sat:nth-child(6)').should('to.contain', 'Sat');
+      cy.get('.fc-day-header.fc-sun:nth-child(7)').should('to.contain', 'Sun');
     });  
 
     it('test if week view display correct dates', function() {
