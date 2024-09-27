be26dca2268ab591e52ee6ba60d8dea40ada6b47
Jeff Reiffers <jeffreiffers@gmail.com>
2024-09-27 13:50:44 +0200

    chore: repair tests and upgrade cypress

--- e2e/cypress/e2e/specs/admin.cy.ts
+++ e2e/cypress/e2e/specs/admin.cy.ts
@@ -7,8 +7,9 @@ describe('Datalandsbyen', function() {
       cy.typeAndRetry("#username", "admin").should('have.value', 'admin');
       cy.typeAndRetry('#password', 'MyPassword').should('have.value', 'MyPassword');    
       cy.get('#login').click();
+      cy.get('span.avatar').should('to.contain', 'A')
     }, () => {
-      cy.url().should('include', '/user/admin') 
+      cy.url().should('include', '/user/admin')      
     })
   });  
   
