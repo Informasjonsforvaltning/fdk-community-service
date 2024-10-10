35bfa34266fbcaf2f79c7bc5d6f2fe6373d701e1
Jeff Reiffers <jeffreiffers@gmail.com>
2024-10-10 15:14:45 +0200

    chore: prod config

--- startup.prod.sh
+++ startup.prod.sh
@@ -2,7 +2,7 @@
 
 echo "Community Docker container has been started"
 
-/setup-msmtp.sh
+setup-msmtp.sh
 
 # Setup a cron schedule (every hour)
 echo "0 * * * * TEST_MODE=$TEST_MODE TEST_EMAIL=$TEST_EMAIL API_TOKEN=$API_TOKEN API_TOKEN_WRITE=$API_TOKEN_WRITE TOKEN_UID=$TOKEN_UID BASE_URL='https://datalandsbyen.norge.no' /run.sh >> /usr/src/app/files/log/cron.log 2>&1
@@ -13,12 +13,9 @@ cron -f &
 
 node ./nodebb activate nodebb-plugin-fdk-resource-link
 node ./nodebb activate nodebb-plugin-fdk-consent
-node ./nodebb activate nodebb-plugin-fdk-sso
-node ./nodebb activate nodebb-plugin-calendar
 node ./nodebb activate nodebb-plugin-dbsearch
-node ./nodebb activate nodebb-plugin-gdpr
-node ./nodebb activate nodebb-plugin-google-analytics
-node ./nodebb reset -t nodebb-theme-fdk
+node ./nodebb activate nodebb-plugin-sso-oauth2-multiple
 
-su node -c "node ./nodebb build"
-su node -c "node ./nodebb start"
+export FORCE_BUILD_BEFORE_START=true
+echo "Running entrypoint.sh"
+entrypoint.sh
