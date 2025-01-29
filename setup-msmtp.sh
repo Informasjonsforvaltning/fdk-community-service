01379f2d6b25eb1457cf06a052b1cb8bc3e8bc91
Hans Olav Sund <hans.olav.sund@digdir.no>
2025-01-29 08:50:03 +0100

    tmp: set to off until further notice (#221)
    * tmp: turn off until further notice

--- setup-msmtp.sh
+++ setup-msmtp.sh
@@ -6,8 +6,8 @@ cat > /etc/msmtprc << EOL
 # Set default values for all following accounts.
 defaults
 port $SMTP_PORT
-tls on
-tls_starttls on
+tls off
+tls_starttls off
 tls_certcheck off
 tls_trust_file $SMTP_TLS_CA
 tls_host_override $SMTP_TLS_HOSTNAME
